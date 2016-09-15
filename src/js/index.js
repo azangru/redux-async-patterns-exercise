e1bb3706e5c40e70e5c84eff176acef9ed04e535
Andrey Azov <andrey.azov@gmail.com>
2016-09-16 01:27:00 +0300

    Adding saga middleware

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,23 @@ import '../styles/styles.scss'; // eslint-disable-line import/no-unresolved
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { Router, browserHistory } from 'react-router';
-
 
 import routes from '~/routes';
 import reducers from '~/state/reducers';
+import rootSaga from '~/state/sagas';
+
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-  reducers
+  reducers,
+  applyMiddleware(sagaMiddleware)
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
