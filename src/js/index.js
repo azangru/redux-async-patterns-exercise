2abfd1e7b40826f59e9274fcad6b68dbe4e21b8c
Andrey Azov <andrey.azov@gmail.com>
2016-09-21 18:42:49 +0300

    Removed code splitting; React now takes over smoothly on the client

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,7 @@ import '../styles/styles.scss'; // eslint-disable-line import/no-unresolved
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { match, browserHistory } from 'react-router';
 import App from './app';
-import routes from '~/routes';
 
-match({history: browserHistory, routes}, (error, redirectLocation, renderProps) => {
-    if (error) {
-        return console.error('BrowserEntry match error', error);
-    }
-    ReactDOM.render(<App {...renderProps} />, document.querySelector('main'));
-});
+
+ReactDOM.render(<App />, document.querySelector('main'));
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,58 +1,27 @@
 import Layout from './layout';
-
-// the solution for imitating System.js on the server is taken from this
-// StackOverflow question: http://stackoverflow.com/questions/37121442/server-side-react-with-webpack-2-system-import
-if (typeof System === 'undefined') {
-    // we are on the server side
-    var System = {
-        import(path) {
-            return Promise.resolve(require(path));
-        }
-    };
-}
-
-function errorLoading(err) {
-    console.error('Could not load the route', err);
-}
-
-function loadRoute(cb) {
-    return (module) => cb(null, module.default);
-}
+import Main from '~/modules/main/Main';
+import Showcase from '~/modules/showcase/Showcase';
+import Login from '~/modules/login/Login';
+import HashtagAutocompletionPage from '~/modules/hashtag-autocompletion-demo/DemoPage';
 
 export default {
     component: Layout,
     path: '/',
     indexRoute: {
-        getComponent(location, cb) {
-            System.import('./modules/main/Main.jsx')
-                .then(loadRoute(cb))
-                .catch(errorLoading);
-        }
+        component: Main
     },
     childRoutes: [
         {
             path: 'showcase',
-            getComponent(location, cb) {
-                System.import('./modules/showcase/Showcase.jsx')
-                    .then(loadRoute(cb))
-                    .catch(errorLoading);
-            }
+            component: Showcase
         },
         {
             path: 'login',
-            getComponent(location, cb) {
-                System.import('./modules/login/Login.jsx')
-                    .then(loadRoute(cb))
-                    .catch(errorLoading);
-            }
+            component: Login
         },
         {
             path: 'hashtag-autocompletion-with-draftjs',
-            getComponent(location, cb) {
-                System.import('./modules/hashtag-autocompletion-demo/DemoPage.jsx')
-                    .then(loadRoute(cb))
-                    .catch(errorLoading);
-            }
+            component: HashtagAutocompletionPage
         }
     ]
 };
