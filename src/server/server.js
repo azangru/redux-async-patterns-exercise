01ac0cd5ba64a3cfcf54adc618d8db2cf4ed7b50
Andrey Azov <andrey.azov@gmail.com>
2016-09-26 16:53:24 +0300

    Added node cluster

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,18 @@
+import cluster from 'cluster';
+import os from 'os';
+
+// Express-related imports
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 
-// for logging purposes
+// Logging-related imports
 import morgan from 'morgan';
 import winston from 'winston';
 import util from 'util';
 import averageCpuUsage from './utils/cpu-usage';
 
-// for react server-side rendering
+// React-related imports
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
@@ -17,12 +21,10 @@ import clientRoutes from '../client/js/routes';
 import configureStore from '../client/js/state/store';
 import waitAll from '../client/js/state/sagas/waitAll';
 
+// Server-side router
 import router from './routes';
 
-let app = express();
-
-// Logging Middleware
-app.use(morgan('dev'));
+// Logging utility
 let winstonLogger = new winston.Logger({
     transports: [
         new (winston.transports.Console)(),
@@ -33,75 +35,101 @@ process.on('uncaughtException', (err) => {
     winstonLogger.error(`UNHANDLED EXCEPTION! ${err}`);
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '../../public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// CREATE A CLUSTER OF NODE PROCESSES
+if (cluster.isMaster) {
+    os.cpus().forEach(() => {
+        cluster.fork();
+    });
+    
+    cluster.on('listening', function(worker, address) {
+        console.log(`Worker started with PID ${worker.process.pid}; listening on port ${address.port}`);
+    });
+    
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+        // Replace the dead worker,
+        // we're not sentimental
+        winstonLogger.error(`Worker ${worker.id} died`);
+        cluster.fork();
+    });
+} else {
+    var app = express();
 
-app.use('/api', router);
+    // Logging Middleware
+    app.use(morgan('dev'));
 
-app.get('*', (req, res) => {
-    // FOR LOGGING PURPOSES
-    const startResponseTimestamp = Date.now();
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
 
-    let store = configureStore(); // creating a new store every new request
-    store.dispatch({
-        type: 'GREET',
-        payload: {message: "Hello world!"}
+    app.use(express.static(path.join(__dirname, '../../public')));
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
     });
 
-    match({ routes: clientRoutes, location: req.url }, (error, redirectLocation, renderProps) => {
-        if (error) {
-          res.status(500).send(error.message);
-        } else if (redirectLocation) {
-            console.log('redirect location');
-        } else if (renderProps) {
-            const preloaders = renderProps.components
-                .filter((component) => component && component.preload)
-                .map((component) => component.preload(renderProps.params, req))
-                .reduce((result, preloaders) => result.concat(preloaders), []);
+    app.use('/api', router);
 
-            store.runSaga(waitAll(preloaders)).done.then(() => {
-                const endApiQueryTimestamp = Date.now();
-                
-                const app = ReactDOMServer.renderToString(
-                    React.createElement(Provider, {store},
-                        React.createElement(RouterContext, renderProps)
-                    )
-                );
+    app.get('*', (req, res) => {
+        // FOR LOGGING PURPOSES
+        const startResponseTimestamp = Date.now();
 
-                // FOR LOGGING PURPOSES
-                const endResponseTimestamp = Date.now();
+        let store = configureStore(); // creating a new store every new request
+        store.dispatch({
+            type: 'GREET',
+            payload: {message: "Hello world!"}
+        });
 
-                averageCpuUsage().then((percentCpuUsage) => {
-                    util.log(`time to response from api: ${endApiQueryTimestamp - startResponseTimestamp} ms; time for rendering: ${endResponseTimestamp - endApiQueryTimestamp} ms; total response time: ${endResponseTimestamp - startResponseTimestamp} ms; memory consumption: ${util.inspect(process.memoryUsage())}; CPU usage: ${percentCpuUsage}%`);
+        match({ routes: clientRoutes, location: req.url }, (error, redirectLocation, renderProps) => {
+            if (error) {
+              res.status(500).send(error.message);
+            } else if (redirectLocation) {
+                console.log('redirect location');
+            } else if (renderProps) {
+                const preloaders = renderProps.components
+                    .filter((component) => component && component.preload)
+                    .map((component) => component.preload(renderProps.params, req))
+                    .reduce((result, preloaders) => result.concat(preloaders), []);
+
+                store.runSaga(waitAll(preloaders)).done.then(() => {
+                    const endApiQueryTimestamp = Date.now();
+
+                    const app = ReactDOMServer.renderToString(
+                        React.createElement(Provider, {store},
+                            React.createElement(RouterContext, renderProps)
+                        )
+                    );
+
+                    // FOR LOGGING PURPOSES
+                    const endResponseTimestamp = Date.now();
+
+                    averageCpuUsage().then((percentCpuUsage) => {
+                        util.log(`time to response from api: ${endApiQueryTimestamp - startResponseTimestamp} ms; time for rendering: ${endResponseTimestamp - endApiQueryTimestamp} ms; total response time: ${endResponseTimestamp - startResponseTimestamp} ms; memory consumption: ${util.inspect(process.memoryUsage())}; CPU usage: ${percentCpuUsage}%`);
+                    });
+
+                    res.render('index.ejs', {app, store: store.getState()});
+                }).catch((e) => {
+                    winstonLogger.error(`ERROR DURING REQUEST HANDLING! — ${e}`);
+                    return res.status(500).send(e.message);
                 });
+            } else {
+              res.status(404).send('not found lol');
+            }
+        });
 
-                res.render('index.ejs', {app, store: store.getState()});
-            }).catch((e) => {
-                winstonLogger.error(`ERROR DURING REQUEST HANDLING! — ${e}`);
-                return res.status(500).send(e.message);
-            });
-        } else {
-          res.status(404).send('not found lol');
-        }
     });
 
-});
+    app.set('port', process.env.PORT || 3000);
 
-app.set('port', process.env.PORT || 3000);
-
-let server = app.listen(app.get('port'), () => {
-    console.log('Express server listening on port ' + server.address().port);
-});
+    let server = app.listen(app.get('port'), () => {
+        console.log('Express server listening on port ' + server.address().port);
+    });
+}
 
 
 export default app;
