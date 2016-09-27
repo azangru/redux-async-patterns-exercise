177589a6d0082dd0515a7a4ef48c19483d35fa82
Andrey Azov <andrey.azov@gmail.com>
2016-09-27 16:05:35 +0300

    Fixed code splitting

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -2,7 +2,13 @@ import '../styles/styles.scss'; // eslint-disable-line import/no-unresolved
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { match, browserHistory } from 'react-router';
 import App from './app';
+import routes from '~/routes';
 
-
-ReactDOM.render(<App />, document.querySelector('main'));
+match({history: browserHistory, routes}, (error, redirectLocation, renderProps) => {
+    if (error) {
+        return console.error('BrowserEntry match error', error);
+    }
+    ReactDOM.render(<App {...renderProps} />, document.querySelector('main'));
+});
