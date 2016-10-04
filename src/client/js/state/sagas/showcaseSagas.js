503ea7097978ab621e06b5f13ad14bd8cef7bbef
Andrey Azov <andrey.azov@gmail.com>
2016-10-04 12:02:48 +0300

    Removed extra import and updated npm script

--- src/client/js/state/sagas/showcaseSagas.js
+++ src/client/js/state/sagas/showcaseSagas.js
@@ -1,5 +1,5 @@
 import { takeEvery } from 'redux-saga';
-import { call, put, select } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
 
 import * as types from '../constants/ActionTypes';
 import {showcaseFetcher, resourcesFetcher} from '~/api/showcase';
